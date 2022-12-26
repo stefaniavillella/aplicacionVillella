b2f55f2f8bc3107d6b009d3aab60aa0c4204164a
unknown <svillella@main.net.ar>
2022-12-26 10:11:11 -0300

    errores en mail

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,21 +1,80 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, Button } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, Button, Alert } from 'react-native'
+import React, { useCallback, useEffect, useReducer, useState } from 'react'
 import colors from '../../constants/colors'
 
 import { useDispatch } from 'react-redux';
 import { signUp } from '../store/actions/auth.action';
+import Input from '../components/Input';
 
 
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+const formReducer = (state, action) => {
+    if(action.type === FORM_INPUT_UPDATE) {
+        const updateValues = {
+            ...state.inputValues,
+            [action.input]: action.isValid
+        };
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        };
+        let updatedFormIsValid = true
+        for(const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities [key]
+        };
+        return {
+            formIsValid: updatedFormIsValid,
+            inputValidities: updatedValidities,
+            inputValues: updateValues,
+        };
+    };
+    return state;
+};
 
 const AuthScreen = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null);
+
+  const [formState, formDispatch] = useReducer(formReducer, {
+    inputValues:{
+        email: "",
+        password: "",
+    },
+    inputValidities:{
+        email: false,
+        password: false,
+    },
+    formIsValid: false,
+  });
+
+
+  useEffect (() => {
+    if (error) {
+        Alert.alert("Ocurrió un error", error, [{text: "Ok"}])
+    }
+  }, [error]);
+
+
 
   const handleSignUp = () => {
     dispatch (signUp(email, password))
-  }
+  };
+
+
+  const onInputChangeHandler = useCallback(
+    (inputIdentifier, inputValue, inputValidity) =>{
+        formDispatch({
+            type: FORM_INPUT_UPDATE,
+            value: inputValue,
+            isValid: inputValidity,
+            input: inputIdentifier,
+        })
+    },
+    [formDispatch],
+  );
+
 
 
   return (
@@ -23,29 +82,34 @@ const AuthScreen = () => {
         <View style={styles.container}>
             <Text style={styles.title}>Dreams Graphics: Login</Text>
             <View>
-                <TextInput style={styles.input}
+                <Input 
                 id="email"
-                placeholder="email"
+                label= "Email"
                 keyboardType="email-address"
+                placeholder="Ingresa tu correo electrónico"
+                required
                 autoCapitalize="none"
-                onChangeText={setEmail}
+                errorText = "Por favor ingresá un email válido"
+                onInputChange={onInputChangeHandler}
                 initialValue=""
                 />
 
-               <TextInput style={styles.input}
+               <Input
                 id="password"
-                placeholder="password"
+                label="Password"
                 keyboardType="default"
+                placeholder="Ingresa tu contraseña"
+                required
                 secureTextEntry
                 minlength={6}
                 autoCapitalize='none'
-                onChangeText={setPassword}
+                //onChangeText={setPassword}
                 initialValue=""
                 />
 
             </View>
 
-            <View>
+            <View style={styles.button}>
                 <Button title='Registrarme' color={colors.inicial} onPress={handleSignUp} />
             </View>
 
@@ -83,5 +147,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
     },
+
+    button:{
+        marginTop:10,
+    }
     
 })
